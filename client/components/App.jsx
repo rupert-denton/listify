7f8777df8ad50e1d739d22a1b857eaafd805de9b
Angela Yang <kyan211@mywhitecliffe.com>
2022-06-16 20:44:36 +1200

    create form.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-import ListContainer from './ListContainer'
+import React, { useState } from 'react'
 import Form from './Form'
 
 function App() {
+  const [list, setList] = useState(['Eggs'])
+
+  function handleAddItem(newItem) {
+    setList([...list, newItem])
+  }
+
   return (
     <>
       <div className="title">
@@ -12,7 +17,12 @@ function App() {
       {/* This 'main' div is only for styling (so we can use flexbox) */}
       <div className="main">
         <div className="page-container">
-          <ListContainer />
+          <Form onAddItem={handleAddItem} />
+          <div className="list-container">
+            <div className="list-item-container">
+              <h3>Your List </h3>
+            </div>
+          </div>
         </div>
       </div>
     </>
