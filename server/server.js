c3c213341887515e02fd39c6562700ccbfe56c77
Rupert <67182833+rupert-denton@users.noreply.github.com>
2022-06-17 14:48:11 +1200

    added engines

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require('express')
 
 const server = express()
 
+const port = process.env.PORT || 3000
+
 server.use(express.static(path.join(__dirname, 'public')))
 
 module.exports = server
